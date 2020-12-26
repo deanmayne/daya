74036a33baa9e0f71738fcbdd2706356d61c17cd
ishafran <isabelle.shafran@gmail.com>
2020-12-26 10:28:18 -0700

    add user login functionality with jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
@@ -10,7 +12,7 @@ router.post("/register", (req, res) => {
 
   User.findOne({ username: req.body.username }).then(user => {
     if (user) {
-      return res.status(400).json({username: "A user has already registered with this address"});
+      return res.status(400).json({username: "User already exists"});
     } else {
       const newUser = new User({
         username: req.body.username,
@@ -28,6 +30,50 @@ router.post("/register", (req, res) => {
       });
     }
   });
+});
+
+// Login User 
+router.post('/login', (req, res) => {
+    // const {errors, isValid } = validateLoginInput(req.body);
+
+    // if(!isValid) {
+    //     return res.status(400).json(errors)
+    // }
+    
+    const username = req.body.username;
+    const password = req.body.password; 
+
+    User.findOne({username})
+        .then(user => {
+            if(!user){
+                // errors.username = 'user not found';
+                // return res.status(400).json(errors);
+                return res.status(400).json('user doesnt exist')
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        const payload = {id: user.id, username: user.username};
+
+                        jwt.sign(
+                            payload, 
+                            keys.secretOrKey, 
+                            {expiresIn: 3600}, 
+                            (err, token) => {
+                                res.json({
+                                    success: true, 
+                                    token: 'Bearer ' + token
+                                });
+                            });
+                    } else {
+                        // errors.password = "Incorrect Password";
+                        // return res.status(400).json(errors);
+                        return res.status(400).json('wrong password')
+                    }
+                });
+        });
+
 });
 
 
