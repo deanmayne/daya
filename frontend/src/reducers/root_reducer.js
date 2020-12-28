7b82abf752e171e97e27650af734a6f8da9c7c7f
Dean Mayne <deancmayne@gmail.com>
2020-12-28 11:57:12 -0700

    add event reducer to root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
+import events from './event_reducer';
 
 const RootReducer = combineReducers({
-    session
+    session,
+    events
 });
 
 export default RootReducer;
