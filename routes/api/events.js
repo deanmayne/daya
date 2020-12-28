60013bdc9ae8861993ea5ca10462b9801e7a9824
ishafran <isabelle.shafran@gmail.com>
2020-12-28 11:55:11 -0700

    add update event

--- routes/api/events.js
+++ routes/api/events.js
@@ -4,5 +4,52 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Event = require ('../../models/Event')
+const validateEventInput = require('../../validation/events');
+
+//   do the get requests need to be protected? how to deal with following situation
+
+
+// Create Event 
+router.post('/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      const { errors, isValid } = validateEventInput(req.body);
+  
+      if (!isValid) {
+        return res.status(400).json(errors);
+      }
+  
+      const newEvent = new Event({
+        title: req.body.title,
+        category: req.body.category,
+        user: req.user.id
+      });
+  
+      newEvent.save().then(event => res.json(event));
+    }
+  );
+
+// Update Event
+  router.patch('/:id',
+    passport.authenticate('jwt', { session: false }),
+    async (req, res) => {
+      const { errors, isValid } = validateEventInput(req.body);
+  
+      if (!isValid) {
+        return res.status(400).json(errors);
+      }
+  
+      const updatedEvent = await Event.findByIdAndUpdate(req.params.id,
+        {
+            title: title || req.body.title, 
+            category: category || req.body.category
+        }, { new: true}
+        );
+
+
+      updatedEvent.save().then(event => res.send(event))
+    }
+  
+  );
 
 
