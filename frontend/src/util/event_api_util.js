1fcf36f70e6fd9637c6e48c3a89b22b3eff3e54c
Dean Mayne <deancmayne@gmail.com>
2020-12-28 12:09:17 -0700

    add event axios calls

--- frontend/src/util/event_api_util.js
+++ frontend/src/util/event_api_util.js
@@ -4,10 +4,19 @@ export const getEvents = () => {
   return axios.get("/api/events");
 };
 
-export const getUserEvents = (id) => {
-  return axios.get(`/api/events/user/${id}`);
+
+export const getUserEvents = (git ) => {
+  return axios.get(`/api/events/user/${user_id}`);
+};
+
+export const getEvent = (event_id) => {
+  return axios.get(`/api/events/${event_id}`);
 };
 
 export const writeEvent = (data) => {
   return axios.post("/api/events/", data);
+};
+
+export const updateEvent = (data) => {
+  return axios.post(`/api/events/${event_id}`, data);
 };
