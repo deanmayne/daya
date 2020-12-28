554d162c615f40694ebb6604453b0571130bb864
ishafran <isabelle.shafran@gmail.com>
2020-12-28 11:07:44 -0700

    add events validations

---
+++ routes/api/events.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Event = require ('../../models/Event')
+
+
