49e5ac3939f12e7f6899376c3eef20c0303fee7a
Jae Doherty <jaeld97@gmail.com>
2020-12-28 15:09:20 -0800

    started splash stying

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -16,35 +16,45 @@ class NavBar extends React.Component {
 
     // Selectively render links dependent on whether the user is logged in
     getLinks() {
-        // if (this.props.loggedIn) {
-        //     return (
-        //         <div>
-        //             <Link to={'/tweets'}>All Tweets</Link>
-        //             <Link to={'/profile'}>Profile</Link>
-        //             <Link to={'/new_tweet'}>Write a Tweet</Link>
-        //             <button onClick={this.logoutUser}>Logout</button>
-        //         </div>
-        //     );
-        // } else {
+        if (this.props.loggedIn) {
             return (
                 <div>
-                    <div id="button-container">
-
-                    <Link to={'/signup'}><button className="user-auth-button">Sign Up</button></Link>
+                    {/* <Link to={'/tweets'}>All Tweets</Link>
+                    <Link to={'/profile'}>Profile</Link>
+                    <Link to={'/new_tweet'}>Write a Tweet</Link> */}
+                    <button onClick={this.logoutUser} className="user-auth-button">Logout</button>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <div id="nav-container">
+                        <h2 id="nav-header">DAYA</h2>
+                        <div id="button-container">
+    
+                        <Link to={'/signup'}><button className="user-auth-button">Sign Up</button></Link>
                         <Link to={'/login'}><button className="user-auth-button">Log In</button></Link>
+                        </div>
+                    </div>
+                    <div id="img-container">
+                        <h1 id="nav-header">the social planner</h1>
+                        <img id="splash-img" alt="pic" src="splash_img.jpg" /> 
                     </div>
                 </div>
             );
-        // }
+        }
     }
 
     render() {
         return (
             <div>
-                <h1>DAYA (navbar header)</h1>
-                { this.getLinks()}
+                <div >
+
+                    
+                    { this.getLinks()}
+                </div>
             </div>
-        );
+        )
     }
 }
 
