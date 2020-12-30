a09c865c4c7f40da1cba89312c30d9bf9f66e182
ishafran <isabelle.shafran@gmail.com>
2020-12-29 19:34:25 -0700

    sign up styling

--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
-import { signup } from '../../actions/session_actions';
+import { signup, login } from '../../actions/session_actions';
 import SignupForm from './signup_form';
+import { withRouter } from  'react-router-dom'
 
 const mapStateToProps = (state) => {
     return {
@@ -15,7 +16,7 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(
+export default withRouter(connect(
     mapStateToProps,
     mapDispatchToProps
-)(SignupForm);
+)(SignupForm));
