25febd520cc0c10d41cc73216ec09fba517086bb
Jae Doherty <jaeld97@gmail.com>
2020-12-31 10:08:33 -0800

    added follow frontend route

---
+++ frontend/src/components/suggested_follows/suggested_follows.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SuggestedFollows extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Suggested Follows</h1>
+            </div>
+        )
+    }
+}
+
+export default SuggestedFollows;
