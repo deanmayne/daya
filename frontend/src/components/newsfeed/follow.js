3fbad40b7d0266484d38929bb0aec704ed98f01d
Jae Doherty <jaeld97@gmail.com>
2020-12-31 11:30:55 -0800

    updating suggested follow routing

--- frontend/src/components/newsfeed/follow.js
+++ frontend/src/components/newsfeed/follow.js
@@ -11,12 +11,16 @@ class Follow extends React.Component {
 
     pickColor() {
       if (this.props.color % 3 === 0) {
-        return ""
+        return 'color-3'
+      } else if (this.props.color % 3 === 1) {
+        return "color-1"
+      } else { 
+        return "color-2"
       }
     }
     render() { 
         return (
-          <div className="follow-card">
+          <div className="follow-card" id={this.pickcolor}>
             <div>
               <h3>Username: <Link id="user-link" to={`/calendar/${this.props.user.username}`}>{this.props.user.username}</Link></h3> 
             </div>
