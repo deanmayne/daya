3e1ddf5596228f48d99e9800a425ea50ed16ffc8
Jae Doherty <jaeld97@gmail.com>
2020-12-31 13:24:37 -0800

    changed newsfeed color scheme

--- frontend/src/components/followers/followers_index.js
+++ frontend/src/components/followers/followers_index.js
@@ -12,19 +12,22 @@ class FollowerIndex extends React.Component {
   render() {
     const { users, followers } = this.props;
     console.log(this.props)
-
+    const followItem = () => {
+      let color = 0;
+      return this.props.users.map((user) => {
+        if (this.props.followers.includes(user._id)) {
+          // this.setState({ color: this.state.color + 1 })
+          color += 1;
+          return (<Follow user={user} color={color} key={user._id} />)
+        };
+      })
+    }
     if (followers.length > 0) {
       return (
         <div>
           <h2>People You're Following</h2>
-          {users.map((user) => {
-              // debugger
-            if(followers.includes(user._id)) {
-            
-              return  <Follow user={user}/>
-        
-            }
-         } )}
+
+          {followItem()}
         </div>
       );
     } else {
